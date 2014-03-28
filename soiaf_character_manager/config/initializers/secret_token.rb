064d213db4f05745a4174c9dc29a193b1f094d8f
jonBarcus <barcus.j@gmail.com>
2014-03-28 16:09:00 -0400

    created new rails application

---
+++ soiaf_character_manager/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SoiafCharacterManager::Application.config.secret_key_base = '7ad673b2d42cbb072ec0cd3fac9c564994afd6d6b757b02d8e497afcd149a99ae47bcbf48e01bfbfa7639d6949c54ed19e7906208b32b2dbacc6df905d9c0147'
---
+++ soiaf_character_manager/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SoiafCharacterManager::Application.config.session_store :cookie_store, key: '_soiaf_character_manager_session'
