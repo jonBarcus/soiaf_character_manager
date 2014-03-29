18e2d023646baa30e1ada7cf022489599d4a06c6
jonBarcus <barcus.j@gmail.com>
2014-03-29 18:42:29 -0400

    Started updated models.
    Made changes to character, house, and user models to establish
    relationships.
    
    Updated the welcome page after user log in to display their
    characters.  Currently not working.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # For Bycrpt.  Will encrypt password and store it in the
   # password digest.  Boohyah.
   has_secure_password
+
+  has_many :houses
+  has_many :characters, :through => :houses
 end
