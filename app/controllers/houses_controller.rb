9cd723b878be529c0309fd4b686b8b1bfae9ba80
jonBarcus <barcus.j@gmail.com>
2014-03-29 13:11:36 -0400

    added houses view with new.html.erb.  Also updated welcome index.html.erb to link to House Creation.  Then updated houses controller to allow for new houses.

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,3 +1,28 @@
 class HousesController < ApplicationController
 
+  def new
+    @regions = Region.all
+    @house = House.new
+  end
+
+  def create
+    @house = House.new(house_params)
+
+    if @house.save
+      redirect_to("/")
+    else
+      render("/houses/new")
+    end
+  end
+
+
+  private
+
+  def house_params
+    params.require(:house).permit(
+      :region_id,
+      :name
+      )
+  end
+
 end
