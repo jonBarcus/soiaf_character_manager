13227792a5df1922703c5c79bfb7aa8b319f3c6e
jonBarcus <barcus.j@gmail.com>
2014-04-01 18:43:52 -0400

    updated seeds.rb file to include admin and normal user accounts for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 Region.delete_all
 House.delete_all
+User.delete_all
 
 the_north = Region.create({name: "The North"})
 the_vale = Region.create({name: "The Vale of Arryn"})
@@ -10,3 +11,11 @@ the_reach = Region.create({name: "The Reach"})
 the_stormlands = Region.create({name: "The Stormlands"})
 dorne = Region.create({name: "Dorne"})
 the_seven_kingdoms = Region.create({name: "The Seven Kingdoms"})
+
+admin_user = User.create({email: "test@test.com", username: "admin",
+                          name: "admin user", password: "admin",
+                          password_confirmation: "admin", is_admin: true})
+
+normal_user = User.create({email: "normal@normal.com", username: "normal",
+                        name: "normal user", password: "normal",
+                        password_confirmation: "normal", is_admin: false})
