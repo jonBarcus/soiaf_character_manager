0606fda9f99e76e1383c8d7a0a17c8e548e5c0fb
jonBarcus <barcus.j@gmail.com>
2014-04-01 18:36:09 -0400

    generated migration to add admin to users table
    Ran migration successfully.  Will next update seeds file to include admin user and normal user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140328211958) do
+ActiveRecord::Schema.define(version: 20140401223257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20140328211958) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "is_admin"
   end
 
 end
