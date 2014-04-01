bd3942068096bd0ee41fa7af4c7d4ddf9e295b2c
jonBarcus <barcus.j@gmail.com>
2014-04-01 12:58:20 -0400

    added export method to characters controller
    Tested successfully with hardcoded username and password for Google.
    
    Deleted those from code.  Aiming to refactor and prompt user for their Google Login information

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -37,6 +37,74 @@ class CharactersController < ApplicationController
 def export
   @character = Character.find_by(id: params[:id])
 
+  google_user = #username here
+  google_pass = #password here
+
+  drive_session = GoogleDrive.login(google_user, google_pass)
+
+  drive_session.create_spreadsheet("main_test")
+
+  session_spreadsheet = drive_session.spreadsheet_by_title("main_test")
+
+  session_worksheet = session_spreadsheet.worksheets[0]
+
+  session_worksheet[1,1] = Time.now
+  session_worksheet[2,1] = "Owner:"
+  session_worksheet[2,2] = current_user.name
+
+  session_worksheet[4,1] = "Name:"
+  session_worksheet[4,2] = @character.name
+  session_worksheet[4,3] = "Age:"
+  session_worksheet[4,4] = @character.age
+  session_worksheet[4,5] = "Gender:"
+  session_worksheet[4,6] = @character.gender
+  session_worksheet[4,7] = "House:"
+  session_worksheet[4,8] = @character.house.name
+  session_worksheet[6,1] = "Agility:"
+  session_worksheet[6,2] = @character.agility
+  session_worksheet[7,1] = "Animal Handling:"
+  session_worksheet[7,2] = @character.animal_handling
+  session_worksheet[8,1] = "Athletics:"
+  session_worksheet[8,2] = @character.athletics
+  session_worksheet[9,1] = "Awareness:"
+  session_worksheet[9,2] = @character.awareness
+  session_worksheet[10,1] = "Cunning:"
+  session_worksheet[10,2] = @character.cunning
+  session_worksheet[11,1] = "Deception:"
+  session_worksheet[11,2] = @character.deception
+  session_worksheet[12,1] = "Endurance:"
+  session_worksheet[12,2] = @character.endurance
+  session_worksheet[13,1] = "Fighting:"
+  session_worksheet[13,2] = @character.fighting
+  session_worksheet[14,1] = "Healing:"
+  session_worksheet[14,2] = @character.healing
+  session_worksheet[15,1] = "Language:"
+  session_worksheet[15,2] = @character.language
+  session_worksheet[6,4] = "Knowledge:"
+  session_worksheet[6,5] = @character.knowledge
+  session_worksheet[7,4] = "Marksmanship:"
+  session_worksheet[7,5] = @character.marksmanship
+  session_worksheet[8,4] = "Persuasion:"
+  session_worksheet[8,5] = @character.persuasion
+  session_worksheet[9,4] = "Status:"
+  session_worksheet[9,5] = @character.status
+  session_worksheet[10,4] = "Stealth:"
+  session_worksheet[10,5] = @character.stealth
+  session_worksheet[11,4] = "Survival:"
+  session_worksheet[11,5] = @character.survival
+  session_worksheet[12,4] = "Thievery:"
+  session_worksheet[12,5] = @character.thievery
+  session_worksheet[13,4] = "Warfare:"
+  session_worksheet[13,5] = @character.warfare
+  session_worksheet[14,4] = "Will:"
+  session_worksheet[14,5] = @character.will
+
+  if session_worksheet.save
+    redirect_to("/characters")
+  else
+    puts "error error error"
+  end
+
 end
 
   private
