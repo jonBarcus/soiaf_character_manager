6173a8761600215d81b13fbd3e8f2b50cbae548d
jonBarcus <barcus.j@gmail.com>
2014-04-15 13:44:48 -0400

    made adjustment in google_oauth module.
    Now appeares to be working, but getting a weird Proc error.
    I've updated the github issue.

--- app/models/google_oauth.rb
+++ app/models/google_oauth.rb
@@ -6,20 +6,24 @@ module GoogleOauth
 
     base_url = "https://accounts.google.com/o/oauth2/token"
     redirect_uri = "http://localhost:3000/oauth"
-    binding.pry
-    headers =   { "user-agent" => "jonathanbarcus",
-                 "content-type" => "application/x-www-form-urlencoded" }
 
-    data =      { code:         params["code"],
-                 redirect_uri:  redirect_uri,
-                 client_id:     GOOGLE_CLIENT_ID,
-                 scope:         params["scope"],
-                 client_secret: GOOGLE_CLIENT_SECRET,
-                 grant_type:    "authorization_code" }
+    headers = { "user-agent" => "jonathanbarcus",
+                "content-type" => "application/x-www-form-urlencoded" }
 
-  # RESPONSE returns hash
-    response = HTTParty.post(base_url, :headers => headers, :body => data)
+    data = { code: params["code"],
+              redirect_uri: redirect_uri,
+              client_id: ENV['GOOGLE_CLIENT_ID'],
+              scope: params["scope"],
+              client_secret: ENV['GOOGLE_CLIENT_SECRET'],
+              grant_type: "authorization_code" }
+
+    response = HTTParty.post(base_url, :headers => headers, :body => data)
 
   end
 
+
 end
+
+
+
+
